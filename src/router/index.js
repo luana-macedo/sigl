fde0bbefe415548823d256d6c183dbd2591fc0cb
Debora Ribeiro Pereira <52083218+rpdeb@users.noreply.github.com>
2021-10-13 11:47:25 -0300

    commit imagens

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import EsqueceuSenha from '../views/EsqueceuSenha.vue'
+import About from '../views/About.vue'
+import Inicio from '../views/Inicio.vue'
+import Professor from '../views/Professor.vue'
+import Salas from '../views/Salas.vue'
+import Disciplinas from '../views/Disciplinas.vue'
+import Academico from '../views/Academico.vue'
+import Subgrupo from '../views/Subgrupo.vue'
+import Periodo from '../views/Periodo.vue'
+import Recuperar from '../views/Recuperar.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +22,59 @@ const routes = [
     component: Home
   },
   {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/esqueceusenha',
+    name: 'EsqueceuSenha',
+    component: EsqueceuSenha
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/inicio',
+    name: 'Inicio',
+    component: Inicio
+  },
+  {
+    path: '/professor',
+    name: 'Professor',
+    component: Professor
+  },
+  {
+    path: '/salas',
+    name: 'Salas',
+    component: Salas
+  },
+  {
+    path: '/periodo',
+    name: 'periodo',
+    component: Periodo
+  },
+  {
+    path: '/disciplinas',
+    name: 'Disciplinas',
+    component: Disciplinas
+  },
+  {
+    path: '/academico',
+    name: 'Academico',
+    component: Academico
+  },
+  {
+    path: '/subgrupo',
+    name: 'Subgrupo',
+    component: Subgrupo
+  },
+  {
+    path: '/Recuperar',
+    name: 'Recuperar',
+    component: Recuperar
   }
 ]
 
