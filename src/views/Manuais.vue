c6830a3715c451c3852ce941683f993e8e9bae05
Aldir Lyra <aldir_lyra@hotmail.com>
2021-10-16 00:32:33 -0300

    Rota de login e Subgrupo

--- src/views/Manuais.vue
+++ src/views/Manuais.vue
@@ -1,16 +1,30 @@
 <template>
   <v-data-table :headers="headers" :items="manuais" class="elevation-2">
     <template v-slot:top>
-      <v-toolbar flat >
+      <v-toolbar flat>
         <v-toolbar-title>Gerenciamento de Manuais</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
-         <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field> 
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Pesquisar"
+          single-line
+          hide-details
+        ></v-text-field>
         <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px"> 
+        <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on, attrs }">
             <!-- <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">Adicionar</v-btn> -->
-            <v-btn class="mx-2 add" fab dark color="green" v-bind="attrs" v-on="on"><v-icon dark> mdi-plus</v-icon></v-btn> 
+            <v-btn
+              class="mx-2 add"
+              fab
+              dark
+              color="green"
+              v-bind="attrs"
+              v-on="on"
+              ><v-icon dark> mdi-plus</v-icon></v-btn
+            >
           </template>
           <v-card>
             <v-card-title>
@@ -18,215 +32,184 @@
             </v-card-title>
 
             <v-card-text>
-               <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col
-           cols="8"
-                    sm="6"
-                    md="4"
-        >
-          <v-text-field
-            v-model="editedItem.descricao"
-            label="Descrição"
-            required
-          ></v-text-field>
-        </v-col>
-        <v-col
-           cols="8"
-                    sm="6"
-                    md="4"
-        >
-          <v-text-field
-            v-model="editedItem.arquivo"
-            label="Arquivo"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
-</v-card-text>
+              <v-form v-model="valid">
+                <v-container>
+                  <v-row>
+                    <v-col cols="8" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.descricao"
+                        label="Descrição"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="8" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.arquivo"
+                        label="Arquivo"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="close"
-              >
+              <v-btn color="blue darken-1" text @click="close">
                 Cancelar
               </v-btn>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="save"
-              >
-                Salvar
-              </v-btn>
+              <v-btn color="blue darken-1" text @click="save"> Salvar </v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
-            <v-card-title class="text-h5">Deseja remover este grupo?</v-card-title>
+            <v-card-title class="text-h5"
+              >Deseja remover este grupo?</v-card-title
+            >
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-btn color="blue darken-1" text @click="closeDelete"
+                >Cancel</v-btn
+              >
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                >OK</v-btn
+              >
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-card>
         </v-dialog>
       </v-toolbar>
     </template>
-       <template v-slot:item.acoes='{item}'>
-       <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-       mdi-message-text
+    <template v-slot:item.acoes="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">
+        mdi-message-text
       </v-icon>
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
+      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
     </template>
     <template v-slot:no-data>
-      <v-btn
-        color="primary"
-        @click="initialize"
-      >
-        Reset
-      </v-btn>
+      <v-btn color="primary" @click="initialize"> Reset </v-btn>
     </template>
   </v-data-table>
 </template>
+
 <style>
-.add{
-  width:5%;
-  height:5%;
+.add {
+  width: 5%;
+  height: 5%;
 }
-body{
-  padding:2%;
+body {
+  padding: 2%;
 }
 </style>
 <script>
-  export default {
-    data: () => ({
-      dialog: false,
-      dialogDelete: false,
-      headers: [
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: "Descrição",
+        align: "start",
+        value: "descricao",
+      },
+      { text: "Arquivo", value: "arquivo" },
+      { text: "Ações", value: "acoes", sortable: false },
+    ],
+    manuais: [],
+    editedIndex: -1,
+    editedItem: {
+      descricao: "",
+      arquivo: "",
+    },
+    defaultItem: {
+      descricao: "",
+      arquivo: "",
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "Cadastrar Manual" : "Editar Dados";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.manuais = [
         {
-          text: 'Descrição',
-          align: 'start',
-          value: 'descricao',
+          descricao: "Manual da Aula",
+          arquivo: "manualdaaula2.pdf",
         },
-        { text: 'Arquivo', value: 'arquivo' },
-        { text: 'Ações', value: 'acoes', sortable: false },
-      ],
-      manuais: [],
-      editedIndex: -1,
-      editedItem: {
-        descricao: '',
-        arquivo: '',
-      },
-      defaultItem: {
-        descricao: '',
-        arquivo: '',
-      },
-    }),
-
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'Cadastrar Manual' : 'Editar Dados'
-      },
+        {
+          descricao: "Manual Anatomia",
+          arquivo: "manualanatomia.pdf",
+        },
+        {
+          descricao: "Manual Clinico",
+          arquivo: "manualclinico.pdf",
+        },
+        {
+          descricao: "Manual ",
+          arquivo: "manual",
+        },
+      ];
+    },
+    editItem(item) {
+      this.editedIndex = this.manuais.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
     },
 
-    watch: {
-      dialog (val) {
-        val || this.close()
-      },
-      dialogDelete (val) {
-        val || this.closeDelete()
-      },
+    deleteItem(item) {
+      this.editedIndex = this.manuais.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
     },
 
-    created () {
-      this.initialize()
+    deleteItemConfirm() {
+      this.manuais.splice(this.editedIndex, 1);
+      this.closeDelete();
     },
 
-    methods: {
-      initialize () {
-        this.manuais = [
-          {
-            descricao: 'Manual da Aula',
-            arquivo: 'manualdaaula2.pdf',
-          },
-          {
-            descricao: 'Manual Anatomia',
-            arquivo: 'manualanatomia.pdf',
-          },
-          {
-            descricao: 'Manual Clinico',
-            arquivo: 'manualclinico.pdf',
-          },
-          {
-            descricao: 'Manual ',
-            arquivo: 'manual',
-          }
-        ]
-      },
-      editItem (item) {
-        this.editedIndex = this.manuais.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
-
-      deleteItem (item) {
-        this.editedIndex = this.manuais.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialogDelete = true
-      },
-
-      deleteItemConfirm () {
-        this.manuais.splice(this.editedIndex, 1)
-        this.closeDelete()
-      },
-
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      closeDelete () {
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.manuais[this.editedIndex], this.editedItem)
-        } else {
-          this.manuais.push(this.editedItem)
-        }
-        this.close()
-      },
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
     },
-  }
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.manuais[this.editedIndex], this.editedItem);
+      } else {
+        this.manuais.push(this.editedItem);
+      }
+      this.close();
+    },
+  },
+};
 </script>
