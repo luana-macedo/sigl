10a6e1d6510b67485263da8be1d9dcf141180523
Aldir Lyra <aldir_lyra@hotmail.com>
2021-10-18 01:28:50 -0300

    Create modal (recuperar senha)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,54 +1,87 @@
 <template>
-<section class="section-form"> 
-         <h1 class="text-h5 title-login">Login</h1>
+  <section class="section-form">
+    <h1 class="text-h5 title-login">Login</h1>
     <v-card width="400" class="mx-auto mt-5 card-login p-1" id="login">
-      <v-card-title class="pb-0">
-      </v-card-title>
+      <v-card-title class="pb-0"> </v-card-title>
+
       <v-card-text>
         <v-form>
-          <v-text-field 
-            label="Usuário" 
-            prepend-icon="mdi-account-circle"
-          />
-          <v-text-field 
-            label="Senha"
-            prepend-icon="mdi-lock"
-          />
+          <v-text-field label="Usuário" prepend-icon="mdi-account-circle" />
+          <v-text-field label="Senha" prepend-icon="mdi-lock" />
         </v-form>
       </v-card-text>
       <v-divider></v-divider>
       <v-card-actions>
-        <v-btn>Esqueceu a Senha ?</v-btn>
+        <v-dialog v-model="recuperar" max-width="400">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn v-bind="attrs" v-on="on"> Esqueceu a senha? </v-btn>
+            <v-spacer></v-spacer>
+          </template>
+
+          <v-card width="400" class="mx-auto mt-5 card-login p-1">
+            <v-card-title class="pb-0"> </v-card-title>
+            <v-card-text>
+              <v-form>
+                <h5 class="text-h5">Recuperação de Senha</h5>
+                <v-text-field
+                  label="E-mail"
+                  prepend-icon="mdi-account-circle"
+                />
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn id="btn-enviar">Enviar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
         <v-btn center id="btn-logar">Login</v-btn>
       </v-card-actions>
     </v-card>
-</section>
+  </section>
 </template>
+
 <style>
-.section-form{
+.section-form {
   padding: 5% 0;
   text-align: center;
-  font-family: 'Roboto','Times New Roman', Times, serif;
+  font-family: "Roboto", "Times New Roman", Times, serif;
 }
-#btn-logar{
-width:50%;
-color:white;
-background-color:#e11b22 ;
-display: block !important;
-padding-inline: 1%;
+#btn-logar {
+  width: 50%;
+  color: white;
+  background-color: #e11b22;
+  display: block !important;
+  padding-inline: 1%;
 }
-.card-login{
+.card-login {
   height: 250px;
 }
-.v-card-actions{
- align-items: center; 
- text-align: center;
- padding-right:2%;
+.v-card-actions {
+  align-items: center;
+  text-align: center;
+  padding-right: 2%;
+}
+#btn-enviar {
+  width: 100%;
+  color: white;
+  background-color: #e11b22;
+  display: block !important;
+  padding-inline: 1%;
+}
+.v-card-actions {
+  align-items: center;
+  text-align: center;
+  padding-right: 2%;
+}
+.text-h5{
+  padding-bottom: 5%;
+  text-align: center;
+  font-family: "Roboto", "Times New Roman", Times, serif;
 }
 </style>
 
 <script>
 export default {
-  name: 'Login',
-}
+  name: "Login",
+};
 </script>
--- src/views/Recuperarsenha.vue
+++ src/views/Recuperarsenha.vue
@@ -1,19 +1,12 @@
 <template>
-<section class="section-form" id="recuperacao"> 
-         <h1 class="text-h5">Recuperação de Senha</h1>
+  <section class="section-form" id="recuperacao">
+    <h1 class="text-h5">Recuperação de Senha</h1>
     <v-card width="400" class="mx-auto mt-5 card-login p-1">
-      <v-card-title class="pb-0">
-      </v-card-title>
+      <v-card-title class="pb-0"> </v-card-title>
       <v-card-text>
         <v-form>
-           <v-text-field 
-            label="Nova Senha"
-            prepend-icon="mdi-lock"
-          />
-          <v-text-field 
-            label="Confirmar Senha"
-            prepend-icon="mdi-lock"
-          />
+          <v-text-field label="Nova Senha" prepend-icon="mdi-lock" />
+          <v-text-field label="Confirmar Senha" prepend-icon="mdi-lock" />
         </v-form>
       </v-card-text>
       <v-divider></v-divider>
@@ -21,33 +14,34 @@
         <v-btn id="btn-salvar">Salvar</v-btn>
       </v-card-actions>
     </v-card>
-    </section>
+  </section>
 </template>
+
 <style>
-.section-form{
+.section-form {
   padding: 5% 0;
   text-align: center;
-  font-family: 'Roboto','Times New Roman', Times, serif;
+  font-family: "Roboto", "Times New Roman", Times, serif;
 }
-#btn-salvar{
-width:100%;
-color:white;
-background-color:#e11b22 ;
-display: block !important;
-padding-inline: 1%;
+#btn-salvar {
+  width: 100%;
+  color: white;
+  background-color: #e11b22;
+  display: block !important;
+  padding-inline: 1%;
 }
-.card-login{
+.card-login {
   height: 250px;
 }
-.v-card-actions{
- align-items: center; 
- text-align: center;
- padding-right:2%;
+.v-card-actions {
+  align-items: center;
+  text-align: center;
+  padding-right: 2%;
 }
 </style>
 
 <script>
 export default {
-  name: 'Recuperacao',
-}
+  name: "Recuperacao",
+};
 </script>
