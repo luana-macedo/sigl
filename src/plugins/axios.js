0dc28cc43a68c98247944df6fac55270d39fe897
Aldir Lyra <aldir_lyra@hotmail.com>
2021-10-25 21:28:39 -0300

    import do axios para toda aplicação

---
+++ src/plugins/axios.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+axios.defaultS.baseURL = 'http://api-sig-itpac-84633.herokuapp.com'
+
+Vue.use({
+    install(Vue) {
+        Vue.prototype.$http = axios
+    }
+})
