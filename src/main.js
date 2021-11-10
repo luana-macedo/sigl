998d636c7065456d34b0d83955c98b47227133d9
Anthony Silveira <anthonysilveira102@gmail.com>
2021-11-09 23:15:57 -0300

    Update Vuelidate

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import vuetify from './plugins/vuetify'
 import VueMask from 'v-mask'
 import router from './router'
 import vSelect from "vue-select"
+import Vuelidate from "vuelidate"
 import "vue-select/dist/vue-select.css"
-
+Vue.use(Vuelidate);
 Vue.component("vue-select", vSelect);
 Vue.use(VueMask);
 Vue.config.productionTip = false
