984fd4766420a9452fbdabda470c4fe4543d1819
LuizzFilipez <89022272+LuizzFilipez@users.noreply.github.com>
2021-11-25 01:05:24 -0300

    começando usuario
    só pro caso da energia cair (o que provavelmente vai acontecer)

---
+++ src/views/Usuario.vue
@@ -0,0 +1,209 @@
+<template>
+  <v-data-table
+    :headers="titulos"
+    :items="usuario"
+    :search="search"
+    class="elevation-2 data-table"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Gerenciamento de Usuário</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Pesquisar"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="400px">
+          <template v-slot:activator="{ on, attrs }">
+            <!-- <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">Adicionar</v-btn> -->
+            <v-btn
+              small
+              class="mx-2 add"
+              fab
+              dark
+              color="green"
+              v-bind="attrs"
+              v-on="on"
+              ><v-icon dark> mdi-plus</v-icon></v-btn
+            >
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ tituloForm }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-form v-model="valid">
+                <v-container>
+                  <v-row>
+                    <v-col cols="8" sm="6" md="4">
+                      <v-text-field
+                        v-model="itemEditado.Status"
+                        label="Status"
+                        :rules="[(v) => !!v || '*Campo Obrigatório*']"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-card-text>
+            
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn small color="warning" dark @click="fechar">
+                Cancelar
+              </v-btn>
+              <v-btn small color="primary" dark @click="save"> Salvar </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDesativar" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5"
+              >Deseja desativar este usuario?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn small color="warning" dark @click="fecharDesativar"
+                >Não</v-btn
+              >
+              <v-btn small color="primary" dark @click="desativeItemConfirm"
+                >Sim</v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:[`item.acoes`]="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)" color="blue">
+        mdi-pencil
+      </v-icon>
+      <v-icon small @click="desativeItem(item)" color="red">
+        mdi-power-standby
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="inicializar"> Reset </v-btn>
+    </template>
+  </v-data-table>
+</template>
+<style>
+.add {
+  width: 40px;
+  height: 40px;
+}
+.template-add {
+  padding-top: 1%;
+}
+.data-table {
+  padding: 3%;
+}
+</style>
+<script>
+export default {
+  data: () => ({
+    search: "",
+    dialog: false,
+    dialogDesativar: false,
+    titulos: [
+      {
+        text: "Status",
+        align: "start",
+        value: "Status",
+      },
+
+      { text: "Ações", value: "acoes" },
+    ],
+    usuario: [],
+    editIndice: -1,
+    itemEditado: {
+      Status: "",
+      arquivo: "",
+    },
+    itemPadrao: {
+      Status: "",
+      arquivo: "",
+    },
+  }),
+
+  computed: {
+    tituloForm() {
+      return this.editIndice === -1 ? "Cadastrar Status" : "Editar Status";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.fechar();
+    },
+    dialogDesativar(val) {
+      val || this.fecharDesativar();
+    },
+  },
+
+  created() {
+    this.inicializar();
+  },
+
+  methods: {
+    inicializar() {
+      this.usuario = [
+        {
+          Status: "Pendente",
+        },
+        {
+          Status: "Aprovado",
+        },
+      ];
+    },
+    editItem(item) {
+      this.editIndice = this.usuario.indexOf(item);
+      this.itemEditado = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    desativeItem(item) {
+      this.editIndice = this.usuario.indexOf(item);
+      this.itemEditado = Object.assign({}, item);
+      this.dialogDesativar = true;
+    },
+
+    desativeItemConfirm() {
+      this.usuario.splice(this.editIndice, 1);
+      this.fecharDesativar();
+    },
+
+    fechar() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.itemEditado = Object.assign({}, this.itemPadrao);
+        this.editIndice = -1;
+      });
+    },
+
+    fecharDesativar() {
+      this.dialogDesativar = false;
+      this.$nextTick(() => {
+        this.itemEditado = Object.assign({}, this.itemPadrao);
+        this.editIndice = -1;
+      });
+    },
+
+    save() {
+      if (this.editIndice > -1) {
+        Object.assign(this.usuario[this.editIndice], this.itemEditado);
+      } else {
+        this.usuario.push(this.itemEditado);
+      }
+      this.fechar();
+    },
+  },
+};
+</script>
